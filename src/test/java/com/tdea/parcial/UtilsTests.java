9f0bbf043d3471fb182bea107aedbca9e2c76b7b
Niver90 <99087333+Niver90@users.noreply.github.com>
2022-03-26 11:31:36 -0500

    Comparacion de horas - Parcial1
    Se compara las horas establecidas asi mismo en 5 pruebas.

--- src/test/java/com/tdea/parcial/UtilsTests.java
+++ src/test/java/com/tdea/parcial/UtilsTests.java
@@ -2,9 +2,43 @@ package com.tdea.parcial;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 public class UtilsTests {
 
     @Test
-    public void blankTest() {}
+    public void horadiatransformartextoNigth() {
+        int hora = 4;
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Night", result);
+    }
+    @Test
+    public void horadiatransformartextoMorning() {
+        int hora = 8;
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Morning", result);
+    }
+    @Test
+    public void horadiatransformartextoAfternoon() {
+        int hora = 16;
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Afternoon", result);
+    }
+    @Test
+    public void horadiatransformartextoEvening() {
+        int hora = 22;
+        String result = Utils.getTimeOfDay(hora);
+        assertEquals("Evening", result);
+    }
+    @Test
+    public void horadiatransformartextoInvalido() {
+        int hora = 28;
+        boolean result = Utils.isValidHour(hora);
+        assertFalse(result);
+    }
 
-}
+
+    }
+
+
